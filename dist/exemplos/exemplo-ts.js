478bb9dbacd94c1d0ef5dc7e45e4dd0f9a35fbd1
Gildo Neto <gildo.neto@thoughtworks.com>
2022-07-07 01:02:17 -0300

    entraga desafios

---
+++ dist/exemplos/exemplo-ts.js
@@ -0,0 +1,37 @@
+"use strict";
+// enum Profissao {
+//   Professora,
+//   Desenvolvedora,
+//   JogadoraDeFutebol
+// }
+// interface Pessoa {
+//   nome: string,
+//   idade: number,
+//   profissao?: Profissao
+// }
+// interface Estudante extends Pessoa {
+//   materias: string[]
+// }
+// const vanessa: Pessoa = {
+//   nome: "Mariana",
+//   idade: 25,
+//   profissao: Profissao.Desenvolvedora
+// }
+// const jess: Estudante = {
+//   nome: "Jessica",
+//   idade: 25,
+//   profissao: Profissao.Desenvolvedora,
+//   materias: ['Math', 'Science']
+// }
+// const Jonah: Estudante = {
+//   nome: "Jonah",
+//   idade: 25,
+//   profissao: Profissao.Desenvolvedora,
+//   materias: ['Math', 'Science']
+// }
+// function listar(lista: string[]) {
+//   for(const item of lista) {
+//     console.log('_ ', item)
+//   }
+// }
+// listar(Jonah.materias)
---
+++ src/desafios/desafio1.ts
@@ -0,0 +1,10 @@
+// Como podemos rodar isso em um arquivo .ts sem causar erros? 
+interface employee {
+  code: number,
+  name: string
+}
+
+const employee: employee = {
+  code: 10,
+  name: "John"
+};
